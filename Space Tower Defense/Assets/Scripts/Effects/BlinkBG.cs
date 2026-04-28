77c5ad7a9484a9395281f4f4df794fa35f683c59
HugoKle <hugo.kleber@elev.ga.lbs.se>
2026-04-28 21:04:57 +0200

    ui start + points placed

---
+++ Space Tower Defense/Assets/Scripts/Effects/BlinkBG.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+
+public class BlinkBG : MonoBehaviour
+{
+   float value = 0f;
+    SpriteRenderer sr;
+    int randomDelay;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        randomDelay = Random.Range(0, 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % randomDelay == 0)
+        {
+            value = Mathf.PingPong(Time.time, 3f) + 0.2f;
+            sr.color = new Color(1f, 1f, 1f, value);
+        }
+    }
+
+}
